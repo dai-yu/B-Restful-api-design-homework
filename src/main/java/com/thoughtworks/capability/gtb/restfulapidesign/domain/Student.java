8ad7cde90ec94d2e8acc5edaaa6d770692345da3
dai-yu <1435480344@qq.com>
2020-11-10 14:39:58 +0800

    [daiyu] create Student domain

---
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/domain/Student.java
@@ -0,0 +1,33 @@
+package com.thoughtworks.capability.gtb.restfulapidesign.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private int id;
+    private String name;
+    private Gender gender;
+    private String note;
+    enum Gender {
+        MALE, FEMALE
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return getId() == student.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
